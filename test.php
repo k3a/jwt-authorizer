0a0c0e233da9b4a6fc591ee210561dade9a83220
Mario Hros <git@reversity.org>
2020-05-16 15:58:35 +0200

    initial

---
+++ test.php
@@ -0,0 +1,35 @@
+<pre>
+<?php
+
+require('./authorizer.php');
+
+//echo "oik";
+print_r(getallheaders());
+
+//htmlspecialchars($_COOKIE["name"])
+
+$jav = new JWTAuthVerifier(["admin-auth.ndemiccreations.com"]);
+
+$user_name = $jav->userName;
+echo "User Name: $user_name\n";
+
+if (!$jav->hasToken()) {
+	echo "No token provided!";
+} else {
+	$gok = $jav->verifyGroup("team");
+	echo "Group ok: ".($gok?"y":"n")."\n";
+
+	$grps = $jav->getGroups();
+	echo "Groups:\n";
+	print_r($grps);
+
+	echo "Mapped groups (nil + strip):\n";
+	$mgrps = $jav->mapGroups(null, true);
+	print_r($mgrps);
+
+	echo "Mapped groups:\n";
+	$mgrps = $jav->mapGroups([
+			"team" => "employee",
+		], true);
+	print_r($mgrps);
+}
