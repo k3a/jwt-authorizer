5e6cab1815eb143cd13f1f1ebf2907f0aaf800d0
Mario Hros <git@reversity.org>
2021-03-08 20:25:37 +0100

    lcobucci/jwt dependency updated and patched to support PHP 8.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '2ea1b9d0ec9ad6c2e29427a3c320e4153b9a4375',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2ea1b9d0ec9ad6c2e29427a3c320e4153b9a4375',
+    ),
+    'lcobucci/jwt' => 
+    array (
+      'pretty_version' => '3.4.5',
+      'version' => '3.4.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '511629a54465e89a31d3d7e4cf0935feab8b14c1',
+    ),
+  ),
+);
